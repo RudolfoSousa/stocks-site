5b626def620952aa08918380f8955d879c84e805
RudolfoSousa <rudolfosousa@gmail.com>
2024-03-26 10:36:21 -0300

    chore: setup envs

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    env: {
+        brapiApiToken: process.env.BRAPI_API_TOKEN,
+        brapiBaseUrl: process.env.BRAPI_BASE_URL
+    }
+};
 
 export default nextConfig;
