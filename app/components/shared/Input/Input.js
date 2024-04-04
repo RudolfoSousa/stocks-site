66eacf59ab5d3229d4113ccfa34a12b539fc225b
RudolfoSousa <rudolfosousa@gmail.com>
2024-04-04 18:57:43 -0300

    chore(`Input`): move component folder

---
+++ app/components/shared/Input/Input.js
@@ -0,0 +1,21 @@
+import { forwardRef } from 'react';
+import styles from './Input.module.css'
+
+const Input = forwardRef((props, ref) => {
+    const { prefix, suffix } = props;
+    return (
+        <div className={styles.InputWrapper}>
+            <label>
+                {prefix && (
+                    <div className={styles.Prefix}>{prefix}</div>
+                )}
+                <input className={styles.Input} {...props} ref={ref} />
+                {suffix && (
+                    <div className={styles.Suffix}>{suffix}</div>
+                )}
+            </label>
+        </div>
+    )
+});
+
+export default Input;
