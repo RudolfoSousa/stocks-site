72f9ba9923a30401aa231e36558f479d219e5469
RudolfoSousa <rudolfosousa@gmail.com>
2024-04-12 16:45:27 -0300

    feat: create `Loading` suggestions component

---
+++ app/components/Searchbar/Suggestions/Loading.js
@@ -0,0 +1,28 @@
+import Skeleton from "../../shared/Skeleton";
+import styles from './Suggestions.module.css'
+
+const Loading = () => {
+    return (
+        <div className={styles.Suggestions}>
+            <Skeleton width={"150px"} height={"20px"} />
+            <ul>
+                <li className={styles.List} >
+                    <div>
+                        <Skeleton width={"60px"} />
+                        <Skeleton width={"80px"} height={"10px"} />
+                    </div>
+                    <Skeleton width={"50px"} height={"10px"} />
+                </li>
+                <li className={styles.List} >
+                    <div>
+                        <Skeleton width={"60px"} />
+                        <Skeleton width={"80px"} height={"10px"} />
+                    </div>
+                    <Skeleton width={"50px"} height={"10px"} />
+                </li>
+            </ul>
+        </div>
+    );
+}
+
+export default Loading;
