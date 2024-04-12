37e8f93f4b677ec17bc96bf4e8dc36e3a2850cde
RudolfoSousa <rudolfosousa@gmail.com>
2024-04-12 16:44:36 -0300

    feat(`Searchbar`): use `OutsideClickHandler` component

--- app/components/Searchbar/Searchbar.js
+++ app/components/Searchbar/Searchbar.js
@@ -1,14 +1,16 @@
 'use client'
-import { useRef } from 'react';
+import { useRef, useState } from 'react';
 import { FaMagnifyingGlass } from "react-icons/fa6";
 import Input from "../shared/Input";
 import Suggestions from './Suggestions';
 import Container from '../shared/Container';
 import styles from './Searchbar.module.css'
 import { useSuggestion } from './useSuggestion';
+import OutsideClickHandler from '../shared/OutsideClickHandler';
 
 const Searchbar = () => {
     const inputRef = useRef(null);
+    const [showSuggestion, setShowSuggestion] = useState(true);
     const [suggestions,
         getSuggestions,
         isIdle,
@@ -21,14 +23,24 @@ const Searchbar = () => {
         getSuggestions(inputRef.current.value);
     }
 
+    const handleFocus = (e) => {
+        e.preventDefault();
+        if (isResolved) {
+            setShowSuggestion(true);
+        }
+    }
+
+    const shouldRenderSuggestions = Boolean(!isIdle && (isResolved || isLoading) && showSuggestion)
+
     return (
         <div className={styles.Searchbar}>
             <Container>
-                <Input suffix={<FaMagnifyingGlass />} onChange={(e) => handleChange(e)} onButtonClick={(e) => handleSearch(e)} placeholder="Pesquisar fundo, ação ou bdr" ref={inputRef} />
-                {!isIdle && (
-                    <Suggestions loading={isLoading} suggestions={isResolved ? suggestions : null} />
-                )}
-
+                <OutsideClickHandler onOutsideClick={() => setShowSuggestion(false)}>
+                    <Input suffix={<FaMagnifyingGlass />} onFocus={(e) => handleFocus(e)} onChange={(e) => handleChange(e)} onButtonClick={(e) => handleSearch(e)} placeholder="Pesquisar fundo, ação ou bdr" ref={inputRef} />
+                    {shouldRenderSuggestions && (
+                        <Suggestions loading={isLoading} suggestions={suggestions} />
+                    )}
+                </OutsideClickHandler>
             </Container>
         </div>
     )
