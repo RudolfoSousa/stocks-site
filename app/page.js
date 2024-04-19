ab523b0dce2bd35bd8f1abdc1d7f7088ea6f7cd1
RudolfoSousa <rudolfosousa@gmail.com>
2024-04-19 11:08:46 -0300

    feat(`page`): add `Header` component

--- app/page.js
+++ app/page.js
@@ -1,10 +1,13 @@
+import Header from "./components/Header";
 import Searchbar from "./components/Searchbar";
+import style from './page.module.css';
 
 export default function Home() {
 
   return (
     <main>
       <div>
+        <Header />
         <Searchbar />
       </div>
     </main>
