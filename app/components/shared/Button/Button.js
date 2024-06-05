a209176c34f3abacff46fc1944d86b31822fa087
RudolfoSousa <rudolfosousa@gmail.com>
2024-06-05 11:00:11 -0300

    feat(`Button`): add variant round

--- app/components/shared/Button/Button.js
+++ app/components/shared/Button/Button.js
@@ -1,13 +1,20 @@
+import classnames from 'classnames';
 import styles from './Button.module.css';
 
 const Button = ({
     children,
     prefix,
     suffix,
+    variant,
     ...rest
 }) => {
+
+    const classNames = classnames(styles.Button, {
+        [styles.Round]: variant === 'round'
+    })
+
     return (
-        <button {...rest} className={styles.Button}>
+        <button {...rest} className={classNames}>
             {prefix && prefix}
             <span>
                 {children}
